f1432fba80a7aa654c306be8866abb2d413fb99d
Lucenko_Alla <lucenko.allo4ka@yandex.ru>
2024-05-19 18:31:32 +0300

    еще архитектура

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,38 @@
 export interface ICard {
     _id: string;
-    name: string;
-    about: string;
-    cost: number;
-    caregory: string;
-    image: URL;
+    title: string;
+    description: string;
+    price: number;
+    category: string;
+    image: string;
 }
 
 export interface IUser {
     _id: string;
-    payment: boolean;
+    payment: string;
     address: string;
     email: string;
     tel: string;
 }
 
 export interface ICardsData {
-    catds: ICard[];
+    cards: ICard[];
     preview: string | null;
+    addCard(card: ICard): void;
+    deleteCard(cardId: string, payload: Function | null): void;
+    updateCard(card: ICard, payload: Function | null): void;
+    clearCards(cards: ICard[]): void;
+    getList(cards: ICard[]): ICard[];
 }
 
-export type TCardInfo = Pick<ICard, 'name' | 'cost'>;
+export interface IUserData {
+    choosePay(data: Record<keyof TUserPayAddress, string>): boolean;
+    getContacts(data: Record<keyof TUserContacts, string>): boolean;
+    checkValidation(data: Record<keyof TUserContacts, string>): boolean;
+    clearUser(data: IUser): void;
+}    
+
+export type TCardInfo = Pick<ICard, 'title' | 'price'>;
 
 export type TUserPayAddress = Pick<IUser, 'payment' | 'address'>;
 
